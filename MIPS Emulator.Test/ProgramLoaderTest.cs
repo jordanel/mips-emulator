50f11f1710cb5a9172d77128378ed2369636db03
jordanel <jordan4162@gmail.com>
2018-05-07 23:24:09 -0400

    ProgramLoader test creation started, test project.json structure changed

--- MIPS Emulator.Test/ProgramLoaderTest.cs
+++ MIPS Emulator.Test/ProgramLoaderTest.cs
@@ -1,5 +1,21 @@
-﻿namespace MIPS_Emulator.Test {
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace MIPS_Emulator.Test {
 	public class ProgramLoaderTest {
+		private ProgramLoader target;
 		
+		[SetUp]
+		public void SetUp() {
+			string dir = Path.GetDirectoryName(typeof(ProgramLoaderTest).Assembly.Location);
+			FileInfo file = new FileInfo($"{dir}/TestProjects/Test1/project.json");
+			
+			target = new ProgramLoader(file);
+		}
+
+		[Test]
+		public void Test() {
+			
+		}
 	}
 }
--- MIPS Emulator/ProgramLoader.cs
+++ MIPS Emulator/ProgramLoader.cs
@@ -1,5 +1,10 @@
-﻿namespace MIPS_Emulator {
+﻿using System.IO;
+
+namespace MIPS_Emulator {
 	public class ProgramLoader {
-		
+
+		public ProgramLoader(FileInfo file) {
+			
+		}
 	}
 }
