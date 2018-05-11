92cdf363e0573b468b26d7d15b90786cf29a2668
jordanel <jordan4162@gmail.com>
2018-05-10 22:43:03 -0400

    More ProgramLoader functionality - using JObjects/JTokens to parse JSON config file

--- MIPS Emulator.Test/ProgramLoaderTest.cs
+++ MIPS Emulator.Test/ProgramLoaderTest.cs
@@ -8,14 +8,16 @@ namespace MIPS_Emulator.Test {
 		[SetUp]
 		public void SetUp() {
 			string dir = Path.GetDirectoryName(typeof(ProgramLoaderTest).Assembly.Location);
-			FileInfo file = new FileInfo($"{dir}/TestProjects/Test1/project.json");
+			FileInfo file = new FileInfo(Path.Combine(dir, "TestProjects/Test1/project.json"));
 			
 			target = new ProgramLoader(file);
 		}
 
 		[Test]
 		public void Test() {
+			Mips mips = target.Mips;
 			
+			Assert.AreEqual(4, mips.Pc);
 		}
 	}
 }
--- MIPS Emulator/ProgramLoader.cs
+++ MIPS Emulator/ProgramLoader.cs
@@ -1,10 +1,56 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.CSharp;
+using Newtonsoft.Json.Linq;
 
 namespace MIPS_Emulator {
 	public class ProgramLoader {
+		public Mips Mips { get; }
+		private string basePath;
 
 		public ProgramLoader(FileInfo file) {
+			this.basePath = file.DirectoryName;
+			this.Mips = LoadMipsFromFile(file);
+		}
+
+		private Mips LoadMipsFromFile(FileInfo file) {
+			string json;
+			using (StreamReader r = file.OpenText()) {
+				json = r.ReadToEnd();
+			}
 			
+			JObject project = JObject.Parse(json);
+			
+			uint pc = ParseNumber(project["programCounter"]);
+			InstructionMemory imem = BuildInstructionMemory(project["imem"]);
+			
+			return new Mips(pc, imem, null);
+		}
+
+		private uint ParseNumber(JToken token) {
+			return ParseNumber((string) token);
+		}
+
+		private uint ParseNumber(string token) {
+			return Convert.ToUInt32(token, 10);
+		}
+
+		private InstructionMemory BuildInstructionMemory(JToken token) {
+			uint[] init = ReadInitFile(token["initFile"]);
+			return null;
+		}
+
+		private uint[] ReadInitFile(JToken token) {
+			string path = (string) token["filepath"];
+			string format = (string) token["format"] ?? "hex";
+
+			using (StreamReader r = new StreamReader(Path.Combine(basePath, path))) {
+				
+			}
+
+			return null;
 		}
 	}
 }
